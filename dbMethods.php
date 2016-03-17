1334baeb6d9ff739ee53e7b3acbedfb263225dd0
dimlyange <dimliang95@gmail.com>
2016-03-17 09:21:50 +0300

    Fixes before presentation for contractor

--- dbMethods.php
+++ dbMethods.php
@@ -63,51 +63,79 @@ function getRouteData($routeId) {
     }
 }
 
+function getAllRoutes() {
+
+    $routesDbName = "Database/routes.json";
+    $routes = json_decode(file_get_contents($routesDbName), true);
+    if (isset($routes)) {
+
+        $routesCount = count($routes);
+        $routesArray = array();
+        for ($routeId = 1; $routeId <= $routesCount; $routeId ++) {
+
+            $currentRoute = Route::withID($routeId);
+            $departureCityName = $currentRoute->getDepartureCity()->getName();
+            $arrivalCityName = $currentRoute->getArrivalCity()->getName();
+            $routeArray = array(
+                "id" => $routeId,
+                "departureCityName" => $departureCityName,
+                "arrivalCityName" => $arrivalCityName
+            );
+            array_push($routesArray, $routeArray);
+        }
+
+        return $routesArray;
+    } else {
+
+        //TODO
+    }
+}
+
 
 // Для Яра
 // функция скачивает информацию о товарах из prodord_tovtozakaz по id заказа из prodord_zakaz
-function getGood($prodordOrderId) {
-
-    $goodDataArray = array();
-
-    $dbHost='localhost';
-    $dbName='xxxx';
-    $dbUser='zzzz';
-    $dbPass='yyyy';
-
-    $db = mysql_connect($dbHost,$dbUser,$dbPass);
-    mysql_select_db($dbName,$db);
-
-    mysql_query("SET NAMES 'utf8'");
-    mysql_query("SET CHARACTER SET 'utf8'");
-    mysql_query("SET SESSION collation_connection = 'utf8_general_ci'");
-
-    $sql = mysql_query("SELECT id, zakaz, tovar, kol, brand, name, srok, delivery, price, comment, cost, roznica, registr, valut, file_name_sh FROM prodord_tovtozakaz WHERE zakaz = $prodordOrderId", $db)
-    or die("Invalid query: " . mysql_error());
-
-    while ($tableRows = mysql_fetch_row($sql)) {
-
-        $goodData = array();
-        $goodData["goodId"] = $tableRows[0];
-        $goodData["zakaz"] = $tableRows[1];
-        $goodData["tovar"] = $tableRows[2];
-        $goodData["kol"] = $tableRows[3];
-        $goodData["brand"] = $tableRows[4];
-        $goodData["name"] = $tableRows[5];
-        $goodData["srok"] = $tableRows[6];
-        $goodData["delivery"] = $tableRows[7];
-        $goodData["price"] = $tableRows[8];
-        $goodData["comment"] = $tableRows[9];
-        $goodData["cost"] = $tableRows[10];
-        $goodData["roznica"] = $tableRows[11];
-        $goodData["registr"] = $tableRows[12];
-        $goodData["valut"] = $tableRows[13];
-        $goodData["file_name_sh"] = $tableRows[14];
-
-        array_push($goodDataArray, $goodData);
-    }
-
-    mysql_close($db);
-
-    return $goodDataArray;
-}
+//function getGood($prodordOrderId) {
+//
+//    $goodDataArray = array();
+//
+//    $dbHost='localhost';
+//    $dbName='xxxx';
+//    $dbUser='zzzz';
+//    $dbPass='yyyy';
+//
+//    $db = mysql_connect($dbHost,$dbUser,$dbPass);
+//    mysql_select_db($dbName,$db);
+//
+//    mysql_query("SET NAMES 'utf8'");
+//    mysql_query("SET CHARACTER SET 'utf8'");
+//    mysql_query("SET SESSION collation_connection = 'utf8_general_ci'");
+//
+//    $sql = mysql_query("SELECT id, zakaz, tovar, kol, brand, name, srok, delivery, price, comment, cost, roznica, registr, valut, file_name_sh FROM prodord_tovtozakaz WHERE zakaz = $prodordOrderId", $db)
+//    or die("Invalid query: " . mysql_error());
+//
+//    while ($tableRows = mysql_fetch_row($sql)) {
+//
+//        $goodData = array();
+//        $goodData["goodId"] = $tableRows[0];
+//        $goodData["zakaz"] = $tableRows[1];
+//        $goodData["tovar"] = $tableRows[2];
+//        $goodData["kol"] = $tableRows[3];
+//        $goodData["brand"] = $tableRows[4];
+//        $goodData["name"] = $tableRows[5];
+//        $goodData["srok"] = $tableRows[6];
+//        $goodData["delivery"] = $tableRows[7];
+//        $goodData["price"] = $tableRows[8];
+//        $goodData["comment"] = $tableRows[9];
+//        $goodData["cost"] = $tableRows[10];
+//        $goodData["roznica"] = $tableRows[11];
+//        $goodData["registr"] = $tableRows[12];
+//        $goodData["valut"] = $tableRows[13];
+//        $goodData["file_name_sh"] = $tableRows[14];
+//
+//        array_push($goodDataArray, $goodData);
+//    }
+//
+//    mysql_close($db);
+//
+//    return $goodDataArray;
+//}
--- main.php
+++ main.php
@@ -9,24 +9,38 @@ require_once("DatabaseClasses/City.php");
 require_once("DatabaseClasses/Route.php");
 
 
-//if (isset($_POST["routeId"]) && isset($_POST["requiredLandmarksNumber"])) {
-//
-//    $routeID = $_POST["routeId"];
-//    $requiredLandmarksNumber = $_POST["requiredLandmarksNumber"];
-//
-//
-//    $currentRoute = Route::withID($routeID);
-//    $currentRoute->setRequiredLandmarksNumber($requiredLandmarksNumber);
-//
-//    $jsonRequiredLandmarks = json_encode($currentRoute);
-//
-//    return $jsonRequiredLandmarks;
-//}
+if (isset($_POST["requestName"])) {
+    switch ($_POST["requestName"]) {
+        case "getFirstRoute":
+            $currentRoute = Route::withID("1");
+            $currentRoute->setRequiredLandmarksNumber("10");
 
+            $jsonRequiredLandmarks = json_encode($currentRoute);
 
+            var_export($jsonRequiredLandmarks);
+            break;
 
-$currentRoute = Route::withID("1");
-$currentRoute->setRequiredLandmarksNumber("2");
+        case "getRoute":
+            if (isset($_POST["routeId"]) && isset($_POST["requiredLandmarksNumber"])) {
 
-$jsonRequiredLandmarks = json_encode($currentRoute);
-var_export($jsonRequiredLandmarks);
+                $routeId = $_POST["routeId"];
+                $requiredLandmarksNumber = $_POST["requiredLandmarksNumber"];
+
+                $currentRoute = Route::withID($routeId);
+                $currentRoute->setRequiredLandmarksNumber($requiredLandmarksNumber);
+
+                $jsonRequiredLandmarks = json_encode($currentRoute);
+
+                var_export($jsonRequiredLandmarks);
+            }
+            break;
+
+        case "getRoutesList":
+
+            $routesArray = getAllRoutes();
+            $jsonRoutesArray = json_encode($routesArray);
+
+            var_export($jsonRoutesArray);
+            break;
+    }
+}
